cc63c6f68149519bfcb6e06adeaacf6ee88cd292
goncalo-a21807061 <a21807061@alunos.ulht.pt>
2020-11-15 17:57:34 +0000

    tentativa testUnit

--- src/pt/ulusofona/lp2/theWalkingDEISIGame/TestTWDGameManager.java
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/TestTWDGameManager.java
@@ -10,20 +10,21 @@ public class TestTWDGameManager {
 
     @Test
     public void test01Move() {
-        /*
-
         //teste movimento normal humano
         TWDGameManager twdGameManager = new TWDGameManager();
         twdGameManager.startGame(new File("dados.txt"));
-        //ver se Humano com ID 1 está na posição (3,3)
+
+        //ver se Humanos e Zombies estao na posicao certa
         assertEquals(1,twdGameManager.getElementId(3,3));
+        assertEquals(2,twdGameManager.getElementId(3,4));
+        assertEquals(3,twdGameManager.getElementId(4,4));
+        ;
         //mover Humano da posição (3,3) para a posicao (3,2)
         twdGameManager.move(3,3,3,2);
         //Verificar se Humano se moveu
         assertEquals(1,twdGameManager.getElementId(3,2));
-
-
-         */
+        assertEquals(2,twdGameManager.getElementId(3,4));
+        assertEquals(3,twdGameManager.getElementId(4,4));
     }
 
 
